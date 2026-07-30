ed6e4e30a4ffbdc309a6b8f02ecb9770aa0d9951
rafiach <rahmadfahrudi321@gmail.com>
2026-07-31 00:04:08 +0700

    edit proxy.ts to make /report become pulic

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,28 +1,39 @@
 import { auth } from "@/lib/auth";
-// import type { AppRole } from "@/lib/auth/modules/authorization/permissions";
 import { NextRequest, NextResponse } from "next/server";
 
 const AUTH_ONLY_PATHS = ["/auth/sign-in", "/auth/sign-up"];
+
+// Route yang boleh diakses tanpa login
+const PUBLIC_PATHS = ["/report"];
+
 const SESSION_COOKIE_NAME =
   process.env.NODE_ENV === "development"
     ? "better-auth.session_token"
     : "__Secure-better-auth.session_token";
-// const ROLE_PROTECTED: { prefix: string; requiredRole: AppRole }[] = [
-//   { prefix: "/dashboard/settings", requiredRole: "admin" },
-//   { prefix: "/dashboard/users", requiredRole: "admin" },
-// ];
 
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const callbackUrl = `${pathname}${request.nextUrl.search}`;
-  const isAuthOnly = AUTH_ONLY_PATHS.some((path) => pathname.startsWith(path));
+
+  const isAuthOnly = AUTH_ONLY_PATHS.some((path) =>
+    pathname.startsWith(path),
+  );
+
+  const isPublic = PUBLIC_PATHS.some((path) =>
+    pathname.startsWith(path),
+  );
+
+  // Route yang memang membutuhkan login
+  const requiresAuth = !isAuthOnly && !isPublic;
+
   const sessionCookie = request.cookies.get(SESSION_COOKIE_NAME)?.value;
 
   if (!sessionCookie) {
-    if (!isAuthOnly) {
+    if (requiresAuth) {
       const url = request.nextUrl.clone();
-      url.searchParams.set("callbackUrl", callbackUrl);
       url.pathname = "/auth/sign-in";
+      url.searchParams.set("callbackUrl", callbackUrl);
+
       return NextResponse.redirect(url);
     }
 
@@ -34,28 +45,25 @@ export async function proxy(request: NextRequest) {
       headers: request.headers,
     });
 
-    if (!session?.session && !isAuthOnly) {
+    if (!session?.session && requiresAuth) {
       const url = request.nextUrl.clone();
-      url.searchParams.set("callbackUrl", callbackUrl);
       url.pathname = "/auth/sign-in";
+      url.searchParams.set("callbackUrl", callbackUrl);
+
       return NextResponse.redirect(url);
     }
-
-    // const sessionRole = (session?.user as { role?: string } | undefined)?.role;
-
-    // if (roleProtectedRoute && sessionRole !== roleProtectedRoute.requiredRole) {
-    //   return NextResponse.redirect(new URL("/", request.url));
-    // }
 
     if (isAuthOnly && session?.session) {
       return NextResponse.redirect(new URL("/", request.url));
     }
   } catch (error) {
     console.error(error);
-    if (!isAuthOnly) {
+
+    if (requiresAuth) {
       const url = request.nextUrl.clone();
-      url.searchParams.set("callbackUrl", callbackUrl);
       url.pathname = "/auth/sign-in";
+      url.searchParams.set("callbackUrl", callbackUrl);
+
       return NextResponse.redirect(url);
     }
   }
