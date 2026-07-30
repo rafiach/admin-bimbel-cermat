896fe71da0aa8b2df45128dcb0799b4be2bee843
rafiach <rahmadfahrudi321@gmail.com>
2026-07-30 23:12:18 +0700

    tes new middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getSessionCookie } from "better-auth/cookies";
+// import { NextRequest, NextResponse } from "next/server";
+// import { getSessionCookie } from "better-auth/cookies";
 
 // export function middleware(request: NextRequest) {
 //   const sessionCookie = getSessionCookie(request);
@@ -22,3 +22,15 @@ import { getSessionCookie } from "better-auth/cookies";
 // export const config = {
 //   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 // };
+
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
