536c769a82aaf072d3e876b4242921ccdc76bf8c
rafiach <rahmadfahrudi321@gmail.com>
2026-07-30 23:08:59 +0700

    tes hide middleware

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
-export function middleware(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request);
-  const { pathname } = request.nextUrl;
+// export function middleware(request: NextRequest) {
+//   const sessionCookie = getSessionCookie(request);
+//   const { pathname } = request.nextUrl;
 
-  const isAuthRoute = pathname.startsWith("/auth");
-  const isPublicRoute = pathname.startsWith("/report");
+//   const isAuthRoute = pathname.startsWith("/auth");
+//   const isPublicRoute = pathname.startsWith("/report");
 
-  if (!sessionCookie && !isAuthRoute && !isPublicRoute) {
-    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
-  }
+//   if (!sessionCookie && !isAuthRoute && !isPublicRoute) {
+//     return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+//   }
 
-  if (sessionCookie && isAuthRoute) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+//   if (sessionCookie && isAuthRoute) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+// };
